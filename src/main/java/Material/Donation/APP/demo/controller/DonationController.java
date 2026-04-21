e1db597dacf4b9f89420463e47d35a7967eb05cc
Mary Thoeun <mary@example.com>
2026-04-21 09:14:06 +0700

    add local

--- src/main/java/Material/Donation/APP/demo/controller/DonationController.java
+++ src/main/java/Material/Donation/APP/demo/controller/DonationController.java
@@ -14,6 +14,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/donations")
+@CrossOrigin(origins = "http://localhost:5173")
 @RequiredArgsConstructor
 public class DonationController {
 
